e4d83f535b320af0ef053dff609c941e3a265697
krishnab5846-dev <krishnab5846@gmail.com>
2026-07-09 21:40:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-document.getElementById("practiceBtn").addEventListener("click", () => {
-  alert("🚀 Practice mode will be added in the next version!");
-});
+// Dashboard Button
+const practiceBtn = document.getElementById("practiceBtn");
+
+if (practiceBtn) {
+  practiceBtn.addEventListener("click", () => {
+    alert("🚀 Practice mode coming soon!");
+  });
+}
+
+// Vocabulary Page
+const wordList = document.getElementById("wordList");
+
+if (wordList && typeof n5Words !== "undefined") {
+
+  displayWords(n5Words);
+
+  const search = document.getElementById("search");
+
+  search.addEventListener("input", () => {
+
+    const value = search.value.toLowerCase();
+
+    const filtered = n5Words.filter(word =>
+      word.japanese.includes(value) ||
+      word.reading.includes(value) ||
+      word.meaning.toLowerCase().includes(value)
+    );
+
+    displayWords(filtered);
+
+  });
+
+}
+
+function displayWords(words) {
+
+  wordList.innerHTML = "";
+
+  words.forEach(word => {
+
+    wordList.innerHTML += `
+
+      <div class="card" style="margin-bottom:20px;">
+
+        <h1>${word.japanese}</h1>
+
+        <h3>${word.reading}</h3>
+
+        <p><strong>Meaning:</strong> ${word.meaning}</p>
+
+        <p><strong>Type:</strong> ${word.type}</p>
+
+      </div>
+
+    `;
+
+  });
+
+}
