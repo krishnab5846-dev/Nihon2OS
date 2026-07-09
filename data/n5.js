f9c3d96d02b71e68cb2c9905969d9f50e8937763
krishnab5846-dev <krishnab5846@gmail.com>
2026-07-09 21:38:26 +0530

    Create n5.js

---
+++ data/n5.js
@@ -0,0 +1,62 @@
+const n5Words = [
+  {
+    japanese: "食べる",
+    reading: "たべる",
+    meaning: "To eat",
+    type: "Verb"
+  },
+  {
+    japanese: "飲む",
+    reading: "のむ",
+    meaning: "To drink",
+    type: "Verb"
+  },
+  {
+    japanese: "行く",
+    reading: "いく",
+    meaning: "To go",
+    type: "Verb"
+  },
+  {
+    japanese: "見る",
+    reading: "みる",
+    meaning: "To see",
+    type: "Verb"
+  },
+  {
+    japanese: "読む",
+    reading: "よむ",
+    meaning: "To read",
+    type: "Verb"
+  },
+  {
+    japanese: "猫",
+    reading: "ねこ",
+    meaning: "Cat",
+    type: "Noun"
+  },
+  {
+    japanese: "犬",
+    reading: "いぬ",
+    meaning: "Dog",
+    type: "Noun"
+  },
+  {
+    japanese: "学校",
+    reading: "がっこう",
+    meaning: "School",
+    type: "Noun"
+  },
+  {
+    japanese: "先生",
+    reading: "せんせい",
+    meaning: "Teacher",
+    type: "Noun"
+  },
+  {
+    japanese: "友達",
+    reading: "ともだち",
+    meaning: "Friend",
+    type: "Noun"
+  }
+];
