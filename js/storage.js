12427786f49d49a4784c697f32918a344715e730
krishnab5846-dev <krishnab5846@gmail.com>
2026-07-09 22:47:50 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,22 @@
+function getFavorites() {
+  return JSON.parse(localStorage.getItem("favorites")) || [];
+}
 
+function saveFavorites(favorites) {
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+function toggleFavourite(word) {
+
+  let favorites = getFavorites();
+
+  if (favorites.includes(word)) {
+    favorites = favorites.filter(w => w !== word);
+  } else {
+    favorites.push(word);
+  }
+
+  saveFavorites(favorites);
+
+  displayWords(currentWords);
+}
