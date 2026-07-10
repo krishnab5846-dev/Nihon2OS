ecef86cebba1af763e10a90733a503f1c1650219
krishnab5846-dev <krishnab5846@gmail.com>
2026-07-10 18:07:42 +0530

    Update vocabulary.js

--- js/vocabulary.js
+++ js/vocabulary.js
@@ -42,10 +42,12 @@ function displayWords(words) {
   wordList.innerHTML = "";
 
   const favorites = getFavorites();
+  const learnedWords = getLearnedWords();
 
   words.forEach(word => {
 
     const saved = favorites.includes(word.japanese);
+    const learned = learnedWords.includes(word.japanese);
 
     wordList.innerHTML += `
       <div class="word-card">
@@ -56,13 +58,7 @@ function displayWords(words) {
 
           <button
             onclick="toggleFavourite('${word.japanese}')"
-            style="
-              background:none;
-              border:none;
-              font-size:28px;
-              cursor:pointer;
-              padding:0;
-            ">
+            style="background:none;border:none;font-size:28px;cursor:pointer;padding:0;">
             ${saved ? "❤️" : "🤍"}
           </button>
 
@@ -70,29 +66,28 @@ function displayWords(words) {
 
         <h3>${word.reading}</h3>
 
-       <p><strong>Meaning:</strong> ${word.meaning}</p>
+        <p><strong>Meaning:</strong> ${word.meaning}</p>
 
-<div style="display:flex;justify-content:space-between;align-items:center;margin-top:15px;">
+        <div style="display:flex;justify-content:space-between;align-items:center;margin-top:15px;">
 
-<div class="badge">${word.type}</div>
+          <div class="badge">${word.type}</div>
 
-<button
-onclick="toggleLearned('${word.japanese}')"
-style="
-background:${getLearnedWords().includes(word.japanese) ? '#16a34a' : '#475569'};
-color:white;
-border:none;
-padding:8px 14px;
-border-radius:8px;
-cursor:pointer;
-font-size:13px;
-">
+          <button
+            onclick="toggleLearned('${word.japanese}')"
+            style="
+              background:${learned ? '#16a34a' : '#475569'};
+              color:white;
+              border:none;
+              padding:8px 14px;
+              border-radius:8px;
+              cursor:pointer;
+            ">
+            ${learned ? "✅ Learned" : "Mark as Learned"}
+          </button>
 
-${getLearnedWords().includes(word.japanese) ? '✅ Learned' : 'Mark as Learned'}
+        </div>
 
-</button>
-
-</div>
+      </div>
     `;
 
   });
